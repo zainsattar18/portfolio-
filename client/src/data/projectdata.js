a525556c14eab03b5d6326764a46a1082f9946a7
zainsattar18 <zainsattar18@gmail.com>
2020-09-07 09:58:27 -0400

    added gifs and new project

--- client/src/data/projectdata.js
+++ client/src/data/projectdata.js
@@ -1,25 +1,26 @@
-import BelayOn from '../images/Belay0n.png'
+import BelayOn from '../images/belay-on.gif'
 import OneWorld from '../images/OneWorld.png'
 import Pokemon from '../images/Pokemon.png'
-import Spork from '../images/Spork.png'
+import Spork from '../images/spork.gif'
+import ZainShop from '../images/zains-shop.gif'
 
 
 const projectData = [
   {
-    title:"PokeDex",
-    stack: "HTML, CSS, JavaScript, Poke-Api, Netlify",
-    description:"Need information about your favorite Pokemon? Look no further. Get stats on over 650+ Pokemon and a brief description all while staying true to the late 90's style. Gotta Catch 'Em All!",
-    image:Pokemon,
-    website_link:"https://pokedex-p1.netlify.app/",
-    github_link: "https://github.com/zainsattar18/PokeDex"
+    title:"Zain's Shop",
+    stack: "React, Redux, Firebase, Stripe, Netlify",
+    description:"Developed an E-Commerce platform which allows to Authenticate using their Email or Google Account (OAuth 2.0) and processes payments using Stripe. Created a Firebase account for user and shop data. React for client side rendering using Redux & updated React practices (Hooks).",
+    image:ZainShop,
+    website_link:"https://zainsgearshop.netlify.app/",
+    github_link: "https://github.com/zainsattar18/zains-shop"
   },
   {
-    title:"OneWorld",
-    stack: "React, CSS, JavaScript, Country-Api, Netlify",
-    description:"OneWorld allows the user to retrieve pertinent information on any country in the world! Start by continent and continue down via sub-continent until you reach your desired location.",
-    image:OneWorld,
-    website_link:"https://one-world-p2.netlify.app/",
-    github_link: "https://github.com/zainsattar18/one-world"
+    title:"Belay-On",
+    stack: "React, Tailwind, Ruby on Rails, PostgreSQL,Heroku, Surge, Netlify",
+    description:"Full-Stack application that allows the user to create an account and then create a new climb that they've recently completed. Other users are able to leave reviews for any previous climb and give it ratings. Climb On!",
+    image:BelayOn,
+    website_link:"http://belayon.surge.sh/",
+    github_link: "https://github.com/zainsattar18/belay-on"
   },
   {
     title:"Spork",
@@ -30,12 +31,20 @@ const projectData = [
     github_link: "https://github.com/kellymakesstuff/spork"
   },
   {
-    title:"Belay-On",
-    stack: "React, Tailwind, Ruby on Rails, PostgreSQL,Heroku, Surge, Netlify",
-    description:"Full-Stack application that allows the user to create an account and then create a new climb that they've recently completed. Other users are able to leave reviews for any previous climb and give it ratings. Climb On!",
-    image:BelayOn,
-    website_link:"http://belayon.surge.sh/",
-    github_link: "https://github.com/zainsattar18/belay-on"
-  }
+    title:"OneWorld",
+    stack: "React, CSS, JavaScript, Country-Api, Netlify",
+    description:"OneWorld allows the user to retrieve pertinent information on any country in the world! Start by continent and continue down via sub-continent until you reach your desired location.",
+    image:OneWorld,
+    website_link:"https://one-world-p2.netlify.app/",
+    github_link: "https://github.com/zainsattar18/one-world"
+  },
+  {
+    title:"PokeDex",
+    stack: "HTML, CSS, JavaScript, Poke-Api, Netlify",
+    description:"Need information about your favorite Pokemon? Look no further. Get stats on over 650+ Pokemon and a brief description all while staying true to the late 90's style. Gotta Catch 'Em All!",
+    image:Pokemon,
+    website_link:"https://pokedex-p1.netlify.app/",
+    github_link: "https://github.com/zainsattar18/PokeDex"
+  }  
 ]
 export default projectData
